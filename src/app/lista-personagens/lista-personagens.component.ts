b45a9bcadc935e2a9436845d59016df56ebe92ec
lucaspimentel <lucaspimenteldb@gmail.com>
2019-11-30 17:46:37 -0300

    Primeiro commitzinho, falta a conexão com o bancao

---
+++ src/app/lista-personagens/lista-personagens.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-personagens',
+  templateUrl: './lista-personagens.component.html',
+  styleUrls: ['./lista-personagens.component.scss']
+})
+export class ListaPersonagensComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/particulas/particulas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-particulas',
+  templateUrl: './particulas.component.html',
+  styleUrls: ['./particulas.component.scss']
+})
+export class ParticulasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
